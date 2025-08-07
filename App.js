62c50937c68e06d1eaf0b4743f735aa02652e0bb
Rakshitha HN <rakshug73@gmail.com>
2025-08-07 12:21:51 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,24 @@
+import { Fragment,useState } from "react";
+import Meals from "./Components/Meals/Meals";
+import Header from "./Components/Layout/Header";
+import Cart from "./Components/Cart/Cart";
+function App() {
+  const [cartIsShown,setCartIsShown]=useState(false);
+  const showCartHandler =() =>{
+    setCartIsShown(true);
+  };
+  const hideCartHandler = ()=>{
+    setCartIsShown(false);
+  };
+  return (
+ <Fragment>
+  {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals/>
+      </main>
+      </Fragment>
+  );
+}
+
+export default App;
